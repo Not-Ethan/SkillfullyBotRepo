e920c823164a7b02bb00a753e11d0f475363de67
Ethan <Ethan@MBA1.local>
2020-06-30 12:17:27 -0400

    basic bot framework

---
+++ server.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs-extra")
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+client.login('token');
