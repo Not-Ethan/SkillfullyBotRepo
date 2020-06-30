a2330c1d9e94f39900055f724ff707adabd63310
Ethan <Ethan@MBA1.local>
2020-06-30 12:26:30 -0400

    small patch

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs-extra")
-const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
