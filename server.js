cbda16826a2a053aa6bec12ed3c3d74db9c12cde
Ethan <Ethan@MBA1.local>
2020-06-30 23:12:28 -0400

    adeed test

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const parser = require("discord-command-parser")
 const axios = require("axios")
 const hyClient = new hypixel("a790f417-f352-461b-9b53-72931a796675")
 const prefix = "s-"
-console.log(token)
 client.on('ready', () => {
     console.log("Ready!")
     client.user.setPresence({
@@ -23,6 +22,13 @@ client.on('ready', () => {
 client.on('message', message => {
     if(!message.content.startsWith(prefix)) return
     if(message.author.bot==true) return
-
+    if(message.content==`${prefix}stats`) {
+        const args = message.content.split(" ").slice(1)
+        const username = args[0]
+        const gamemode = args[1]
+    }
+    if(message.author.id==""&&message.content=="testtrigger") {
+        message.reply("test confirm");
+    }
 }); 
 client.login(token);
