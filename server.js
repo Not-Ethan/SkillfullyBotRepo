c7c01889018e2e801028d9b76836c41f6dfbfb0a
Ethan <Ethan@MBA1.local>
2020-07-02 15:47:01 -0400

    added bedwars segment

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const axios = require("axios")
 const token = process.env.TOKEN;
 const hyClient = new hypixel("a790f417-f352-461b-9b53-72931a796675")
 const prefix = "s-"
+function capitalize(s) {
+    if(typeof s != "string") return
+    return s.charAt(0).toUpperCase + s.slice(1);
+}
 client.on('ready', () => {
     console.log(`Ready! ${client.user.tag}`);
-    client.channels.cache.get("722173404995780708").send("jdjdasj")
     client.user.setPresence({
         activity: {
             name: "Skillfully Guild"
@@ -19,20 +22,74 @@ client.on('ready', () => {
         type: "WATCHING"
     })
 });
-
 client.on('message', (message) => {
+    const gamemodes = {
+        bw: "Bedwars",
+        sw: "Skywars",
+        sb: "Skyblock"
+    }
+    const gamemodes2 = ["Bedwars", "Skywars", "Skyblock"]
     if(message.author.bot==true) return
     if(message.content.startsWith(`${prefix}j`)) {
         const args = message.content.split(" ").slice(1)
         const username = args[0]
-        console.log(username)
-        const gamemode = args[1]
-        message.channel.send("j")
+        let gamemode = args[1]
+        if(!gamemode) return message.channel.send(`You need to specify a gamemode ${message.author}.`)
+        if(!gamemodes.hasOwnProperty(gamemode.toLowerCase())&&!gamemodes2.includes(capitalize(gamemode))) return message.channel.send(`Please choose a proper gamemode, ${message.author}.`)
+        if(gamemodes.hasOwnProperty(gamemode.toLowerCase())) {
+        gamemode = gamemodes[gamemode.toLowerCase()]
+        }
+        let embed;
         hyClient.getPlayer("name", username).then(player=>{
-            console.log(player)
-        }).catch(
+            let base;
+            embed = new Discord.MessageEmbed()
+            .setColor("#6119a8")
+            .setFooter("© 2020 skillfully guild", "https://i.ibb.co/GMmBzLY/blue-and-purp.png")
+            .setThumbnail("https://i.ibb.co/GMmBzLY/blue-and-purp.png")
+            .setTitle(`Stats for ${username} in \`${gamemode}\``)
+            if(gamemode == "Bedwars") {
+                base = player.player.stats["Bedwars"]
+                embed
+                
+                .addField("Level/Stars:", player.player.achievements.bedwars_level)
+                .addField("Final Kills",  base["final_kills_bedwars"], true)
+                .addField("Final Deaths",  base["final_deaths_bedwars"], true)
+                .addField("FKDR", Math.round(1000*(base.final_kills_bedwars/base.final_deaths_bedwars))/1000, true)
+                .addField("\u200b", "\u200b")
+                .addField("Total Beds Broken", base.beds_broken_bedwars, true)
+                .addField("BBLR", Math.round(1000*(base.beds_broken_bedwars/base.losses_bedwars))/1000, true)
+                .addField("\u200b", "\u200b")
+                .addFields({name: "WLR", value: Math.round(1000*(base.wins_bedwars/base.losses_bedwars))/1000, inline: true})
+                .setTimestamp()
+                .setAuthor(message.guild.me.displayName, message.guild.me.user.avatarURL(), null)
+                return embed
+        } else if(gamemode == "Skywars") {
+                function sw_xp_to_lvl(xp) {
+                let xps = [0, 20, 70, 150, 250, 500, 1000, 2000, 3500, 6000, 10000, 15000];
+                if(xp >= 15000) {
+                    return (xp - 15000) / 10000 + 12;
+                } else {
+                    for(let i = 0; i < xps.length; i++) {
+                        if(xp < xps[i]) {
+                            return 1 + i + (xp - xps[i-1]) / (xps[i] - xps[i-1]);
+                        }
+                    }
+                }
+            }
+            console.log(player.player.stats.SkyWars)
+            return embed
+        } else if(gamemode=="Skyblock") {
+
+        }
+
+        }).then( j=>
+            message.channel.send(j)
+        )
+        .catch(
             error=>{
-                console.log(error)
+
+                if(error) console.log(error)
+                    message.channel.send("An error occured, are you sure that player exists?")
             }
         )
     }
